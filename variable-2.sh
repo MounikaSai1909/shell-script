7a7225af58eaea78267e56dceb2d7ccbeae391f5
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-05-07 21:21:35 +0530

    some-message

---
+++ variable-2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+echo " Ramesh :: Hello Suresh,How are you "
+echo " Suresh :: Hi Ramesh ,I am fine. How are you "
+echo " Ramesh :: I am fine too,How is your work "
+echo " Suresh :: not bad .I am thinking to learn new skills"
