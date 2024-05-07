45ca7caf04956c1d78044d672f5d0568ed596e00
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-05-07 21:28:35 +0530

    some-message

--- variable-2.sh
+++ variable-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PERSON1=Ramesh
-PERSON2=Suresh
-echo " Ramesh :: Hello Suresh,How are you "
-echo " Suresh :: Hi Ramesh ,I am fine. How are you "
-echo " Ramesh :: I am fine too,How is your work "
-echo " Suresh :: not bad .I am thinking to learn new skills"
+PERSON1=Sachin
+PERSON2=Rahul
+echo " $PERSON1 :: Hello $PERSON2,How are you "
+echo " $PERSON2 :: Hi $PERSON1 ,I am fine. How are you "
+echo " $PERSON1 :: I am fine too,How is your work "
+echo " $PERSON2 :: not bad .I am thinking to learn new skills"
