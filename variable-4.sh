ccf520009f5f531759732399fbb049386d683297
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-05-07 21:54:13 +0530

    some-message

---
+++ variable-4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "please enter Username::"
+read -s USERNAME #here USERNAME is a variable
+echo " please enter password:: "
+read -s PASSWORD
+echo "Username is : $USERNAME, password is : $PASSWORD"
