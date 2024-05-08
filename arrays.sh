a038cc7e2792ad479a124eccef42e06c4919de64
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-05-08 10:59:39 +0530

    some-message

---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("kushi" "eega" "MCA")
+
+echo "First Movie is : ${MOVIES[0]}"
+echo "First Movie is : ${MOVIES[1]}"
+echo "First Movie is : ${MOVIES[2]}"
+echo "First Movie is : ${MOVIES[@]}"
