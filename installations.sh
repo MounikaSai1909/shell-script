335ecb970642e75bd638e89ae9730785af7af122
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-05-08 18:56:49 +0530

    some-message

--- installations.sh
+++ installations.sh
@@ -4,9 +4,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "please run this script with root access."
-exit 0 #manually exits if error comes
+exit 1 #manually exits if error comes
 else
 echo "you are super user."
 fi
-dnf install mysql -y
+dnf install mysqll -y
+
+if [ $? -ne 0 ]
+then 
+echo "Installation of mysql....Failure"
+exit 1
+fi
  
