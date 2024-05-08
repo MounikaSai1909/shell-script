c841d3ee7af892575117eba6e37b99fc35326e09
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-05-08 18:34:28 +0530

    some-message

--- installations.sh
+++ installations.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-USERID=$(id -u)
 
-if[$USERID -ne 0]
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
 then
-echo " please run this script with root access "
+echo "please run this script with root access."
 else
-echo " it is a superuser "
+echo "you are super user."
 fi
 dnf install mysql -y
+ 
