7543fb191245f77f9269c30a3c6143e836ebf773
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-05-09 17:54:37 +0530

    some-message

--- functions-2.sh
+++ functions-2.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "," -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -16,10 +16,10 @@ VALIDATE(){
 }
 if [ $USERID -ne 0 ]
 then
-echo "please run this script with root access."
-exit 1 #manually exits if error comes
+    echo "please run this script with root access."
+    exit 1 #manually exits if error comes
 else
-echo "you are super user."
+    echo "you are super user."
 fi
 dnf install mysql -y
 VALIDATE $? "Installing MySQL"
