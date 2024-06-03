14e2fa3f1131943b6fc6b7a9a8b9c43c342130bf
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-06-03 10:55:56 +0530

    some-message

--- current-script.sh
+++ current-script.sh
@@ -5,6 +5,6 @@ COURSE="Devops from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./other-script.shell
+#./other-script.shell
 
 echo "After calling other script, course: $COURSE"
