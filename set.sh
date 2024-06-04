7e7917159d086cfa0152975750960bc41b410bd7
MounikaSai1909 <mounikasaiaarepu09@gmail.com>
2024-06-04 14:14:51 +0530

    some-message

--- set.sh
+++ set.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
+
+failure(){
+    echo " Failed at $1: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
 USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
 then
 echo "please run this script with root access."
